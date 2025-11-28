5943b3ea645956f63ae7beb137df545968e9629f
MennaZaid <mennatallahzaid@aucegypt.edu>
2025-11-28 22:00:57 +0200

    fixed some hex files

--- tests/Milestone3/lw.asm
+++ tests/Milestone3/lw.asm
@@ -1,5 +1,4 @@
- auipc x1, 0           # Get current PC
-    addi x1, x1, 20       # Point to data area
+   addi x1, x1, 20       # Point to data area
     lw x2, 0(x1)          # Load word
     lw x3, 4(x1)          # Load another word
     lw x4, 8(x1)          # Load third word
--- tests/Milestone3/sw.asm
+++ tests/Milestone3/sw.asm
@@ -1,7 +1,6 @@
-auipc x1, 0           # Get current PC
+
     addi x1, x1, 32       # Point to storage area
-    lui x2, 0xDEADB
-    addi x2, x2, 0x7EF    # Create value
+    addi x2, x2, 10    # Create value
     sw x2, 0(x1)          # Store word
     sw x2, 4(x1)          # Store at offset 4
     lw x3, 0(x1)          # Read back
